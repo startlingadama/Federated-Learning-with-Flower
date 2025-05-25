a96bb27c357a37701fef7565593eb4f38e3abdf6
Adama COULIBALY <64993975+startlingadama@users.noreply.github.com>
2025-05-25 20:14:41 +0100

    update

--- server/server.py
+++ server/server.py
@@ -2,11 +2,11 @@ import flwr as fl
 
 strategy = fl.server.strategy.FedAvg(
     fraction_fit=1.0,
-    fraction_eval=1.0,
+    fraction_evaluate=1.0,
     min_fit_clients=2,
-    min_eval_clients=2,
+    min_evaluate_clients=2,
     min_available_clients=2,
-    eval_fn=None,
+    evaluate_metrics_aggregation_fn=None
 )
 
 if __name__ == "__main__":
@@ -14,5 +14,5 @@ if __name__ == "__main__":
         server_address="[::]:8080", 
         config={"num_rounds": 3},
         strategy=strategy,
-        enable_dashboard=True,
+        enable_dashboard=True
         )
